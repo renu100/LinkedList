e63c29fbb718d34b3810ffdcef0f9185e5219263
renu100 <renu.chandraker08@gmail.com>
2021-10-05 13:40:45 +0530

    create a

--- main/java/com/LinkedList/MyNode.java
+++ main/java/com/LinkedList/MyNode.java
@@ -1,7 +1,27 @@
 package com.LinkedList;
 
 public class MyNode<K> {
-	public static void main(String[] args) {
-		System.out.println("Welcome");
+	private MyNode next;
+	private K key;
+
+	public MyNode(K key) {
+		this.key = key;
+		this.next = null;
+	}
+
+	public K getKey() {
+		return key;
+	}
+
+	public void setKey(K key) {
+		this.key = key;
+	}
+
+	public MyNode getNext() {
+		return next;
+	}
+
+	public void setNext(MyNode next) {
+		this.next = next;
 	}
 }
--- test/java/com/LinkedList/MyNodeTest.java
+++ test/java/com/LinkedList/MyNodeTest.java
@@ -1,5 +1,20 @@
 package com.LinkedList;
 
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
 public class MyNodeTest {
+
+	@Test
+	void given3NumbersWhenLinkedShouldPassedLinkedListTest() {
+		MyNode<Integer> myFirstNode = new MyNode<>(56);
+		MyNode<Integer> mySecondNode = new MyNode<>(30);
+		MyNode<Integer> myThirdNode = new MyNode<>(70);
+		myFirstNode.setNext(mySecondNode);
+		mySecondNode.setNext(myThirdNode);
+		boolean result = myFirstNode.getNext().equals(mySecondNode) && mySecondNode.getNext().equals(myThirdNode);
+		assertTrue(result);
+	}
 
 }
