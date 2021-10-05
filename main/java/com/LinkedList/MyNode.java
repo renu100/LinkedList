7c910555eb788bab7dcf4010d63ec9418d8feafe
renu100 <renu.chandraker08@gmail.com>
2021-10-05 14:22:20 +0530

    Create Linked List by adding

--- main/java/com/LinkedList/MyNode.java
+++ main/java/com/LinkedList/MyNode.java
@@ -1,7 +1,7 @@
 package com.LinkedList;
 
-public class MyNode<K> {
-	private MyNode next;
+public class MyNode<K> implements INode<K> {
+	private INode next;
 	private K key;
 
 	public MyNode(K key) {
@@ -17,11 +17,11 @@ public class MyNode<K> {
 		this.key = key;
 	}
 
-	public MyNode getNext() {
+	public INode getNext() {
 		return next;
 	}
 
-	public void setNext(MyNode next) {
+	public void setNext(INode next) {
 		this.next = next;
 	}
 }
